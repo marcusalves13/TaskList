966337e73a769881b3607334450fe31a0e35809f
marcusalves13 <78316674+marcusalves13@users.noreply.github.com>
2022-04-06 22:07:18 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,9 @@ export function Header() {
   return (
     <header className="header">
       <div>
-        <img src="/logo.svg" alt="to.do"/>
+       <!-- <img src="/logo.svg" alt="to.do"/> -->
+        <span>to.do</span>
+        span
       </div>
     </header>
   )
